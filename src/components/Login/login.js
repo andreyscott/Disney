08f61e37ca355ec4399a2bd6f46fc0197c17054b
andreyscott <62455784+andreyscott@users.noreply.github.com>
2021-10-06 21:26:10 +0100

    finished creating login page for the disney-clone

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -4,19 +4,17 @@ const Login = (props) => {
     return (
         <Container>
         <content>
+        <CTA>
+        <CTALogoOne src='/images/cta-logo-one.svg' alt="logo"/>
+        <SignUp>GET SOME THERE</SignUp>
+        <Description> Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1. </Description>
+            <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+        </CTA>
 
+<BgImage />
         <h1>Login page</h1>
-        <form>
-            <label>
-            Email:
-            <input type="email" name="email" />
-            </label>
-            <label>
-            Password:
-            <input type="password" name="password" />
-            </label>
-            <button>Login</button>
-        </form>
         </content>
         </Container>
     );
@@ -45,6 +43,83 @@ const content = styled.div`
     padding:  80px 40px;
     height: 100%;
 
+`;
+
+const BgImage = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: top, cover, no-repeat;
+    background-image: url("https://www.streamingrant.com/wp-content/uploads/2021/06/40-best-disney-plus-shows.jpg");
+    z-index: -2;
+`;
+
+const CTA = styled.div`
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    flex-direction: column;
+    margin-bottom: 2vw;
+    max-width: 650px;
+    min-height: 100vh;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top:  0;
+    transition-timing-function: ease-in-out;
+    transition: opacity 0.2s;
+    width: 100%;
+
+    `; 
+
+    const CTALogoOne = styled.img`
+    display: block;
+    max-height: auto;
+    min-height: 1px;
+    margin-bottom: 12px;
+    max-width: 600px;
+    width: 100%;
+    `;
+
+    const SignUp = styled.a`
+    display: block;
+    font-size: 18px;
+    font-weight: bold;
+    color: #f9f9f9;
+    text-align: center;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    padding: 16px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    transition: all 0.2s;
+
+    &:hover {
+        background-color: #0483ee;
+        border: 1px solid #0063e5;
+        cursor: pointer;
+    }
+
+    `;
+    const  Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
 `;
 
     export default Login;
